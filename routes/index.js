42f22a982fc1c709640dc514891c8683d48a45e2
Melissa Pizarro <mpizarro03@gmail.com>
2018-05-22 11:52:25 -0600

    my first route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome to the Jungle :) :) :)' });
 });
 
+router.get("/hello", function(req, res, next) {
+  res.json({hello: "world"})
+
+})
+
 module.exports = router;
